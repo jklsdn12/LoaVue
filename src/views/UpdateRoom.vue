ba47e0b2094f65ffbd0e9ebaa24d6b8a2e84666f
cmmu <jklsdn12@gmail.com>
2022-09-20 17:59:24 +0900

    수정 추가

---
+++ src/views/UpdateRoom.vue
@@ -0,0 +1,158 @@
+<template>
+  <form class="form-signin">
+    <img class="mb-4" :src="`${dgImgUrl}`" alt="" width="144" height="144">
+    <h1 class="h3 mb-3 font-weight-normal">{{DGNAME}}</h1>
+    <label for="inputEmail" class="sr-only">캐릭터</label>
+
+    <label  class="sr-only">날짜/시간</label>
+    <div>
+      <datepicker v-model="picked"
+                  :clearable="true"
+                  :inputFormat="inputFormat"/>
+    </div>
+    <select v-model="param.HOUR"  ref="HOUR"  name="시간">
+      <option v-for="(item, index) in dataHour" :value="item.value" v-bind:key="index" >
+        {{item.id}}
+      </option>
+    </select>
+    &nbsp;
+    <select v-model="param.MIN"  ref="MIN"  name="분">
+      <option  v-for="(item2, index) in dataMin"  :value="item2.value" v-bind:key="index">
+        {{item2.id}}
+      </option>
+    </select>
+    <p/>
+    <label  class="sr-only">비고</label>
+    <input type="text"  class="form-control" required=""  v-model="param.DOCTXT"  ref="DOCTXT"  name="비고">
+
+    <button class="btn btn-lg btn-primary btn-block" type="button" @click="saveRoom">저장</button>
+    <button class="btn btn-lg btn-primary btn-block" type="button" @click="gotoList">리스트</button>
+    <p class="mt-5 mb-3 text-muted"></p>
+  </form>
+</template>
+<style>
+
+</style>
+
+<script>
+import Datepicker from 'vue3-datepicker';
+import { ref } from 'vue'
+import moment from 'moment';
+
+export default {
+  components: {
+    Datepicker
+  },
+  name: "CreateRoom",
+  dgImg(){
+    return this.$store.state.getDgIcon;
+  },
+  data() {
+    return {
+      param : {
+        HOUR: "",
+        MIN: "",
+        DOCTXT: "",
+        STDATE: "",
+        STTIME: "",
+        SEQNO: ""
+      },
+      charData : this.$store.state.charData,
+      dgData :[],
+      dgImgUrl : "",
+      dataHour : this.$store.state.dataHour,
+      dataMin : this.$store.state.dataMin,
+      picked: ref(new Date()),
+      dgtype:"",
+      DGNAME:""
+    };
+  },
+  props: {
+    inputFormat: {
+      type: String,
+      required: false,
+      default: 'yyyyMMdd',
+    }
+  },
+  validate(key, val) {
+    const errors = []
+
+    if (!val) {
+      errors.push(`${key} field is required`)
+    }
+    return errors
+  },
+  methods: {
+    setDgImg(dgType){
+      const list = this.$store.state.dgIcon;
+      for(let i=0; i<list.length; i++){
+        if(list[i].CODTYPE == 'MSTDG' && list[i].CODNUM == dgType) this.dgImgUrl = this.$store.state.servUrl+list[i].IMGSRC;
+      }
+    }
+    ,
+    async saveRoom(){
+      this.param.STDATE = moment(this.picked).format('YYYYMMDD');
+      this.param.STTIME = this.param.HOUR+this.param.MIN+"00";
+
+      if(!this.chkObj(this.param)) return;
+
+      await this.$callAPI("/userRoom/json/updateRoom.data", 'post', this.param
+          , (res) => {
+            if(res && res.data){
+              if(res.data=="S"){
+                alert("저장에 성공하였습니다.");
+                this.$router.push('/');
+              }else if(res.data == "E3"){
+                alert("하나의 레이드에 두개의 캐릭을 넣을 수 없습니다.");
+              }else if(res.data == "E4"){
+                alert("인원 제한을 초과하였습니다.");
+              }else{
+                alert("에러 발생");
+              }
+            }else{
+              alert("알 수 없는 에러 발생");
+            }
+          });
+    },
+    chkObj(obj){
+      let key ="";
+      for (key  in obj) {
+        if(!obj[key]){
+          this.$refs[key].focus();
+          alert(this.$refs[key].name + "을 입력해주세요.");
+          return false;
+        }else if(!this.param.STDATE || this.param.STDATE =='Invalid date'){
+          alert("날짜를 입력해주세요.");
+          return false;
+        }
+      }
+      return true;
+    },
+    gotoList(){
+      this.$router.push('/');
+    }
+  },
+  created() {
+    this.param.SEQNO = this.$route.query.seqno;
+    this.dgtype = this.$route.query.dgtype;
+    this.DGNAME = this.$route.query.dgname;
+    this.param.HOUR = this.$route.query.hour;
+    this.param.MIN = this.$route.query.min;
+    this.param.STDATE = this.$route.query.stdate;
+    this.param.DOCTXT = this.$route.query.doctxt;
+    this.picked = new Date(this.$route.query.stdate);
+
+    // eslint-disable-next-line
+    const reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
+    this.param.STDATE = this.$route.query.stdate.replace(reg,"");
+  },
+  mounted() {
+    if(!this.dgtype  || this.dgtype  == ""){
+      this.dgImgUrl = require(`@/assets/loacon.jpg`);
+    }else{
+      this.setDgImg(this.dgtype);
+    }
+  }
+};
+</script>
+
